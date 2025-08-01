ace740bb46f2197c53d07e967ea057ff8a926b38
hwelch-fle <hwelch@finelines-engineering.com>
2025-07-31 20:54:46 -0400

    add SQLClause class

--- src/arcpie/cursor.py
+++ src/arcpie/cursor.py
@@ -4,6 +4,7 @@ from typing import (
     TypedDict,
     Any,
     Literal,
+    NamedTuple,
 )
 
 from arcpy import (
@@ -49,12 +50,33 @@ EditToken = Literal[
 CursorToken = Literal[GeneralToken | ShapeToken | EditToken]
 CursorTokens: tuple[CursorToken, ...] = CursorToken.__args__
 
+class SQLClause(NamedTuple):
+    """Wrapper for Cursor sql_clause attribute,
+    
+    Arguments:
+        prefix (str): The SQL prefix to be prepended to the `FROM` part of the statment
+        postfix (str): The SQL postfix that will be appended to the `WHERE` clause
+
+    Format:
+        `SELECT {prefix} {fields} FROM {table} WHERE {where_clause} {postfix}`
+
+    Usage:
+        ```python
+            >>> five_longest = SQLClause(prefix='TOP 5', postfix='ORDER BY LENGTH DESC')
+            >>> fc_result = feature_class.get_tuples(('NAME', 'LENGTH'), sql_clause=five_longest))
+            >>> print(list(fc_result))
+            [('foo', 1001), ('bar', 999), ('baz', 567), ('buzz', 345), ('bang', 233)]
+        ```
+    """
+    prefix: str
+    postfix: str
+
 class SearchOptions(TypedDict, total=False):
     """Optional parameters for SearchCursors"""
     where_clause: str
     spatial_reference: str | int | SpatialReference
     explode_to_points: bool
-    sql_clause: tuple[str, str]
+    sql_clause: SQLClause
     datum_transformation: str
     spatial_filter: Geometry
     spatial_relationship: SpatialRelationship
@@ -70,7 +92,7 @@ class UpdateOptions(TypedDict, total=False):
     where_clause: str
     spatial_reference: str | int | SpatialReference
     explode_to_points: bool
-    sql_clause: tuple[str, str]
+    sql_clause: SQLClause
     skip_nulls: bool
     null_value: dict[str, Any]
     datum_transformation: str
