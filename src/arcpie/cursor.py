5392438dc18d3b8ba9f237aa50bfdbb12c2e3545
hwelch-fle <hwelch@finelines-engineering.com>
2025-07-31 20:54:59 -0400

    add SearchOptions docstring

--- src/arcpie/cursor.py
+++ src/arcpie/cursor.py
@@ -72,7 +72,35 @@ class SQLClause(NamedTuple):
     postfix: str
 
 class SearchOptions(TypedDict, total=False):
-    """Optional parameters for SearchCursors"""
+    """Optional parameters for SearchCursors
+    
+    Arguments:
+        where_clause (str): A SQL query that is inserted after the SQL `WHERE` (`SELECT {prefix} {fields} FROM {table} WHERE {where_clause} {postfix}...`)
+        spatial_reference (str | int | SpatialReference): Perform an on the fly projection of the yielded geometry to this reference
+        explode_to_points (bool): Return a row per vertex in each feature (e.g. `[SHAPE, 'eric', 'idle'] -> [Point, 'eric', 'idle'], [Point, 'eric', 'idle'], ...`)
+        sql_clause (SQLClause): A tuple of SQL queries that is inserted after the SQL 
+            `SELECT` and `WHERE` clauses (`SELECT {prefix} {fields} FROM {table} WHERE {where_clause} {postfix}...`)
+        datum_transformation (str): The transformation to use during projection if there is a datum difference between the feature projection and the 
+            target SpatialReference (you can use `arcpy.ListTransformations` to find valid transformations)
+        spatial_filter (Geometry): A shape that will be used to test each feature against using the specified `spatial_relationship` (`'INTERSECTS'`)
+            by default.
+        spatial_relationship (SpatialRelationship): The type of relationship with the `spatial_filter` to test for in each row. Only rows with shapes
+            that match this relationship will be yielded.
+        search_order (SearchOrder): Run the `where_clause {sql_clause}` (`'ATTRIBUTEFIRST'` default) or `spatial_filter` (`'SPATIALFIRST'`) first.
+            This can be used to optimize a cursor. If you have a complex `where_clause`, consider switching to `'SPATIALFIRST'` to cut down on the number
+            of records that the `where_clause` runs for. These two operations are done as seperate SQL operations and `JOINED` in the result
+
+    Returns:
+        ( dict ): A dictionary with the populated keys
+
+    Usage:
+        ```python
+            >>> options = SearchOptions(where_clause='OBJECTID > 10')
+            >>> not_first_ten = feature_class.get_tuples(['NAME', 'LENGTH'], **options)
+            >>> print(list(not_first_ten))
+            [('cleese', 777), ('idle', 222), ('gilliam', 111), ...]
+        
+    """
     where_clause: str
     spatial_reference: str | int | SpatialReference
     explode_to_points: bool
