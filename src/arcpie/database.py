f49847107762546d502dfd833e76cf6670f778bb
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2025-10-01 14:37:58 -0400

    create base Dataset object

--- src/arcpie/database.py
+++ src/arcpie/database.py
@@ -2,8 +2,62 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from .featureclass import FeatureClass
+from .featureclass import (
+    Table,
+    FeatureClass,
+    GeometryType,
+)
 
-class FileGeodatabase:
+from arcpy import (
+    Polygon,
+    PointGeometry,
+    Polyline,
+    Multipoint,
+    Multipatch,
+)
+
+from arcpy.da import (
+    Walk,
+)
+
+class Dataset:
     def __init__(self, conn: str|Path) -> None:
-        self.conn = conn
+        self.conn = Path(conn)
+        self._datasets: dict[str, Dataset] | None = None
+        self._feature_classes: dict[str, FeatureClass[GeometryType]] | None=None
+        self._tables: dict[str, Table] | None=None
+        self.walk()
+
+    @property
+    def datasets(self) -> dict[str, Dataset]:
+        return self._datasets or {}
+    
+    @property
+    def feature_classes(self) -> dict[str, FeatureClass[GeometryType]]:
+        return self._feature_classes or {}
+
+    @property
+    def tables(self) -> dict[str, Table]:
+        return self._tables or {}
+
+    def walk(self) -> None:
+        self._feature_classes = {}
+        for g_type in (PointGeometry, Polyline, Polygon, Multipoint, Multipatch):
+            g_type_string = g_type.__name__ if g_type.__name__ != 'PointGeometry' else 'Point'
+            for root, ds, fcs in Walk(str(self.conn), datatype=['FeatureClass'], type=g_type_string):
+                root = Path(root)
+                if ds:
+                    self._datasets = self._datasets or {}
+                    self._datasets.update({d: Dataset(root / d) for d in ds})
+                else:
+                    self._feature_classes.update({fc: FeatureClass[g_type](root / fc) for fc in fcs})
+        self._tables = {}
+        for root, ds, tbls in Walk(str(self.conn), datatype=['Table']):
+            root = Path(root)
+            self._tables.update({tbl: Table(root / tbl) for tbl in tbls})
+    
+    def __getitem__(self, key: str) -> FeatureClass[GeometryType] | Table | Dataset:
+        ret = self.tables.get(key) or self.feature_classes.get(key) or self.datasets.get(key)
+        if not ret:
+            raise KeyError(f'{key} is not a child of {self.conn.stem}')
+        return ret
