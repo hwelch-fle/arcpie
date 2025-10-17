92ac3b0509da029c9ddf8522f3a0ea1ab7823580
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2025-10-17 12:35:02 -0400

    add lyrx export utility to utils

--- src/arcpie/utils.py
+++ src/arcpie/utils.py
@@ -1,5 +1,7 @@
 """Module for internal utility functions to share between modules"""
 from __future__ import annotations
+from pathlib import Path
+import json
 
 from .featureclass import (
     Table,
@@ -9,6 +11,10 @@ from .featureclass import (
 
 from .database import (
     Dataset,
+)
+
+from .project import (
+    Project
 )
 
 def nat(val: str) -> tuple[tuple[int, ...], tuple[str, ...]]:
@@ -56,3 +62,41 @@ def get_subtype_counts(gdb: Dataset, drop_empty: bool=False) -> dict[str, dict[s
         if (counts := get_subtype_count(fc))
         or not drop_empty
     }
+    
+def export_project_lyrx(project: Project, out_dir: Path, *, indent: int=4, sort: bool=False, skip_errors: bool=False) -> None:
+    """Pull all layers from a project file and output them in a directory as lyrx files
+    
+    Args:
+        project (Project): The `arcpie.Project` instance to export
+        out_dir (Path|str): The target directory for the layer files
+        indent (int): Indentation level of the ouput files (default: 4)
+        sort (bool): Sort the output file by key name (default: False)
+        skip_errors (bool): Skip any layers that fail to be converted to LYRX (default: False)
+    
+    Raises:
+        (JSONDecodeError) : If the `skip_errors` flag is not set, otherwise the error is printed
+    
+    Usage:
+        ```python
+        export_project_lyrx(arcpie.Project('<path/to/aprx>'), '<path/to/output_dir>')
+        ```
+    
+    Note:
+        Output structure will match the structure of the project:
+        `Map -> Group -> Layer`
+        Where each level is a directory. Group Layers will have a directory entry with individual
+        files for each layer they contain, as well as a single layerfile that contains all their 
+        child layers.
+    """
+    out_dir = Path(out_dir)
+    for map in project.maps:
+        map_dir = out_dir / map.name
+        for layer in map.layers:
+            out_file = (map_dir / layer.longName).with_suffix('.lyrx')
+            out_file.parent.mkdir(parents=True, exist_ok=True)
+            try:
+                out_file.write_text(json.dumps(layer.lyrx, indent=indent, sort_keys=sort), encoding='utf-8')
+            except json.JSONDecodeError as e:
+                if not skip_errors:
+                    raise e
+                print(f'Failed to write {map.name}->{layer.longName}: {e}')
