df588d8a1b7fe24c502474fd4ab8271a51a748e7
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-07-10 12:58:36 -0400

    ruff fmt arcpie.toolbox

--- src/arcpie/toolbox.py
+++ src/arcpie/toolbox.py
@@ -1,22 +1,25 @@
 from __future__ import annotations
 
+import datetime as dt
 import inspect
 import time
 import traceback
-from abc import ABC
 from collections.abc import Callable
+from cProfile import Profile
 from datetime import datetime
 from functools import wraps
 from importlib import import_module, reload
+from io import StringIO
 from logging import Logger
+from pstats import Stats
 from traceback import format_exc
 from types import MappingProxyType, ModuleType
 from typing import Any
 
-from arcpie._types import ParameterDatatype
 from arcpie.parameters import Parameter, Parameters
 from arcpie.parameters.custom import *  # noqa: F403
 from arcpie.project import Map, Project
+from arcpie.types import ParameterDatatype
 from arcpie.utils import print
 
 
@@ -55,10 +58,6 @@ class Tool(ToolABC):
 
 
 def profile(*selectors: str):
-    from cProfile import Profile
-    from io import StringIO
-    from pstats import Stats
-
     def wrapper(func: Callable[..., None]) -> Callable[..., None]:
         @wraps(func)
         def inner(*args: Any, **kwargs: Any):
@@ -122,15 +121,15 @@ def safe_load(tools: dict[str, list[str]],
               scope: dict[str, Any] | None = None,
               reload_module: bool = False) -> list[type[ToolABC]]:
     """Safely load in tools to a toolbox placing all failed imports in a `Broken Tools` category
-    
+
     Args:
         tools (dict[str, list[str]]): A mapping of tool modules to tool files or tool classes in a module
         scope (dict[str, Any]): The `globals()` dict for the Toolbox scope (required so loading happend in Toolbox scope)
         reload_module (bool): Reload the module after importing (default: False)
-    
+
     Returns:
         ( list[type[ToolABC]] ): A list of tool classes
-    
+
     Note:
         To isolate bugs in a large toolbox, it is reccomended that you use file/module level importing
         with matching toolclass names (e.g. `Tool.py -> class Tool`) where the Tool mapping is:
@@ -139,11 +138,11 @@ def safe_load(tools: dict[str, list[str]],
             `Tools = {'tools.Tool': ['Tool']}`
         This allows the import to fail softly on a single tool instead of breaking imports for all other
         toolclasses in that `Tool.py` module.
-        
+
         Any tools with errors will be placed in a `Broken Tools` category in the toolbox with the full
         stack trace placed in its description field. The final component of the exception will be placed
         at the end of the tool label for easy debugging.
-    
+
     Example:
         ```python
         >>> # File import
@@ -151,7 +150,6 @@ def safe_load(tools: dict[str, list[str]],
         ...     # import matching class from named file in a submodule (tools)
         ...     # where ToolFileA is ToolFileA.py with a class ToolFileA
         ...     'tools': ['ToolFileA', 'ToolFileB'],
-        ...     
         ...     # Import explicit ToolClass from toolfile (MyTools.py)
         ...     # NOT RECOMMENDED
         ...     'tools.MyTools': ['ToolClassA', 'ToolClassB'],
@@ -159,20 +157,20 @@ def safe_load(tools: dict[str, list[str]],
         >>> tools = safe_import(globals(), Tools)
         ```
     """
-    _tools = [
+    tb_tools = [
         _get_tool(module, tool_name, reload_module)
         for module in tools
         for tool_name in tools[module]
     ]
     if scope:
-        scope.update({tool.__name__: tool for tool in _tools})
-    return _tools
+        scope.update({tool.__name__: tool for tool in tb_tools})
+    return tb_tools
 
 
 # toolify wrapper
 # NOTE: inspect.Parameter is much different from arcpy.Parameter, module level import used to keep them clear
 def _build_params(params: MappingProxyType[str, inspect.Parameter], types: ParameterTypeMap) -> Parameters:
-    _parameters = Parameters()
+    parameters = Parameters()
     for name, param in params.items():
 
         # Build a simple parameter if only type is given, otherwise use the provided Parameter object
@@ -191,10 +189,10 @@ def _build_params(params: MappingProxyType[str, inspect.Parameter], types: Param
                 datatype=param_type,  # pyright: ignore[reportArgumentType]
             )
             p.value = param.default
-        _parameters.append(p)
+        parameters.append(p)
 
     # Match parameter order to types order
-    return Parameters([_parameters[name] for name in types if name in _parameters] + [p for p in _parameters if p.name not in types])
+    return Parameters([parameters[name] for name in types if name in parameters] + [p for p in parameters if p.name not in types])
 
 
 def _read_params(arcpy_params: Parameters | list[Parameter], func_params: MappingProxyType[str, inspect.Parameter], types: ParameterTypeMap) -> tuple[tuple[Any], dict[str, Any]]:
@@ -222,21 +220,21 @@ def toolify(*tool_registries: list[type[ToolABC]],
             logger: Logger | None = None,
 ):
     """Convert a typed function into a tool for the specified Toolbox class
-    
+
     Args:
         *tool_registries (list[type[ToolABC]]): The tool registry lists to add this tool to
         name (str): The name of the tool
-        params (ParameterTypeMap): A mapping of parameter names to Parameter types and a callable constructor 
+        params (ParameterTypeMap): A mapping of parameter names to Parameter types and a callable constructor
             that converts the parameter to the expected value for the function parameter. You can also pass
             a fully formed arcpy.Parameter object as the first item in the tuple instead of a simple type
         debug (bool): Print the converted arguments to the ArcGIS Pro message console (default: False)
         logger (Logger|None): An optional logger to use for logging all runs of the toolified tool
-    
+
     Usage:
         ```python
         >>> @toolify(
-        >>>     TOOL_REGISTRY, 
-        >>>     name='PDF Exporter', 
+        >>>     TOOL_REGISTRY,
+        >>>     name='PDF Exporter',
         >>>     params={
         >>>         'project': ('DEFile', lambda p: Project(p.valueAsText)),
         >>>         'outfile': ('DEFile', lambda p: Path(p.valueAsText))
@@ -253,9 +251,9 @@ def toolify(*tool_registries: list[type[ToolABC]],
             return func(*args, **kwargs)
 
         # Build the tool class
-        _label = func.__name__.replace('_', ' ').title()
-        _description = func.__doc__ or 'No Description Provided'
-        _class_name = _label.replace(' ', '')
+        label = func.__name__.replace('_', ' ').title()
+        description = func.__doc__ or 'No Description Provided'
+        class_name = label.replace(' ', '')
         sig = inspect.signature(func)
         sig_params = sig.parameters
 
@@ -271,22 +269,22 @@ def toolify(*tool_registries: list[type[ToolABC]],
                 res = _execute(*args, **kwargs)
                 end = time.time()
                 if logger:
-                    logger.info(f'[{datetime.isoformat(datetime.now())}] PASS "{self.label}" ({end - start:0.2f} seconds) [{res}]')
+                    logger.info(f'[{datetime.isoformat(datetime.now(tz=dt.UTC))}] PASS "{self.label}" ({end - start:0.2f} seconds) [{res}]')
             except Exception as e:
                 print(f'Something went wrong!:\n\t{traceback.format_exc()}', severity='ERROR')
                 end = time.time()
                 if logger:
-                    logger.info(f'[{datetime.isoformat(datetime.now())}] FAIL "{self.label}" ({end - start:0.2f} seconds) [{e}] ')
+                    logger.info(f'[{datetime.isoformat(datetime.now(tz=dt.UTC))}] FAIL "{self.label}" ({end - start:0.2f} seconds) [{e}] ')
 
         def _local_build_params(self: ToolABC) -> Parameters | list[Parameter]:
             return _build_params(sig_params, params or {})
 
         def _local_init(self: ToolABC) -> None:
-            self.label = name or _label
-            self.description = _description
+            self.label = name or label
+            self.description = description
 
-        _tool_class = type(
-            _class_name,
+        tool_class = type(
+            class_name,
             (ToolABC, ),
             {
                 '__init__': _local_init,
@@ -296,8 +294,8 @@ def toolify(*tool_registries: list[type[ToolABC]],
         )
 
         for registry in tool_registries:
-            if _tool_class.__name__ not in map(lambda c: c.__name__, registry):
-                registry.append(_tool_class)
-                globals()[_class_name] = _tool_class
+            if tool_class.__name__ not in (c.__name__ for c in registry):
+                registry.append(tool_class)
+                globals()[class_name] = tool_class
         return _execute
     return _builder
